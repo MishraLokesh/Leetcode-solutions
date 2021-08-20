be20c7a20e81ad754241e7be9322800b764c5a66
Lokesh Mishra <4man.mishra@gmail.com>
2021-08-20 12:00:42 +0530

    Time: 68 ms (82.17%), Space: 31 MB (41.39%) - LeetHub

--- count-items-matching-a-rule/count-items-matching-a-rule.cpp
+++ count-items-matching-a-rule/count-items-matching-a-rule.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        int count = 0;
-        for(int i=0;i<items.size();i++) {
-            vector<string> v = items[i];
-            if(ruleKey=="type" and (v[0] == ruleValue)) count++;
-            else if(ruleKey=="color" and (v[1] == ruleValue)) count++;   
-            else if(ruleKey=="name" and (v[2] == ruleValue)) count++;
-        }
-        return count;
+    int countMatches(vector<vector<string>>& items, string key, string val) {
+        // int count = 0;
+        // for(int i=0;i<items.size();i++) {
+        //     vector<string> v = items[i];
+        //     // if(ruleKey=="type" and (v[0] == ruleValue)) count++;
+        //     // else if(ruleKey=="color" and (v[1] == ruleValue)) count++;   
+        //     // else if(ruleKey=="name" and (v[2] == ruleValue)) count++;
+        //     if(ruleKey=="type" and (items[i][0] == ruleValue)) count++;
+        //     else if(ruleKey=="color" and (items[i][1] == ruleValue)) count++;   
+        //     else if(ruleKey=="name" and (items[i][2] == ruleValue)) count++;
+        // }
+        // return count;
+    
+        int idx = key == "type" ? 0 : key == "color" ? 1 : 2;
+        return count_if(begin(items), end(items), [&](const auto &i) { return i[idx] == val; });
     }
+    
 };
 
